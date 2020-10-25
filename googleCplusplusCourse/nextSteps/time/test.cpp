6a876bec9d2c9918b571613500ff5b0538545218
Antonio Jimenez <ajimenez@rti.com>
2020-10-25 12:02:32 +0100

    google course next steps: time

---
+++ googleCplusplusCourse/nextSteps/time/test.cpp
@@ -0,0 +1,60 @@
+// Description: A simple time class.
+
+#include <iostream>
+using namespace std;
+
+class Time
+{
+private:
+    int hours_;
+    int minutes_;
+    int seconds_;
+
+public:
+    Time()
+    {
+        hours_ = 0;
+        minutes_ = 0;
+        seconds_ = 0;
+    }
+    void set(int h, int m, int s);
+    void increment();
+    void display();
+};
+
+void Time::set(int h, int m, int s)
+{
+    hours_ = h;
+    minutes_ = m;
+    seconds_ = s;
+}
+
+void Time::increment()
+{
+    seconds_++;
+    minutes_ += seconds_ / 60;
+    hours_ += minutes_ / 60;
+    seconds_ %= 60;
+    minutes_ %= 60;
+    hours_ %= 24;
+    return;
+}
+
+void Time::display()
+{
+    cout << (hours_ % 12 ? hours_ % 12 : 12) << ':'
+         << (minutes_ < 10 ? "0" : "") << minutes_ << ':'
+         << (seconds_ < 10 ? "0" : "") << seconds_
+         << (hours_ < 12 ? " AM" : " PM") << endl;
+}
+
+int main()
+{
+    Time timer;
+    timer.set(23, 59, 58);
+    timer.display();
+    for (int i = 0; i < 500; i++) {
+        timer.increment();
+        timer.display();
+    }
+}
